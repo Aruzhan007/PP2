741ef2976dce7a408002a408759e17613dff9e8f
Aruzhan007 <tlekovaaru@gmail.com>
2018-01-16 12:55:17 +0600

    isPrime added

---
+++ isPrime/lab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab
+{
+    class MainClass
+    {
+        static bool isPrime(int x){
+            for (int i = 2; i <=Math.Sqrt(x); i++)
+                if (x % i == 0) return false;
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++) {
+                Console.WriteLine(args[i]);
+            }
+                //if (isPrime(int.Parse(args[i])))
+               //     Console.WriteLine(args[i]);
+            Console.ReadKey();
+        }
+    }
+}
+
+// csc lab1.cs
+// lab1.exe 4 5 6 
+
