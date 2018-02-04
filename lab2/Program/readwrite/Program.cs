872d219e8bff1338b6b1b54e1680a2b7b06c1766
aruzhantlek <tlekovaaru@gmail.com>
2018-02-04 22:47:34 +0600

    lab2

---
+++ lab2/Program/readwrite/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace readwrite
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"/Users/aruzan/Desktop/Calculus/a.txt");
+            string ss = sr.ReadToEnd();
+            sr.Close();
+            string[]arg = ss.Split();
+            int a = 999999;
+            foreach(string s in arg)
+            {
+                bool b = true;
+                    for (int i = 2; i < Math.Sqrt(int.Parse(s)); i++)
+                        if (int.Parse(s) % i == 0)
+                            b = false;
+                    if (b == true && int.Parse(s) != 1)
+                        a = Math.Min(a, int.Parse(s));
+              
+                }
+
+            StreamWriter sw = new StreamWriter(@"/Users/aruzan/Desktop/Calculus/b.txt");
+            sw.WriteLine(a);
+            sw.Close();
+        }
+    }
+}
