f3c619f1b6337bac3cc86236b2600abc787638bc
aruzhantlek <tlekovaaru@gmail.com>
2018-02-05 00:57:55 +0600

    newFD

---
+++ lab2/FileDirectory/FileDirectory/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FileDirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo(@"/Users/aruzan/Documents/programming1");
+            DirectoryInfo[] dget = directory.GetDirectories();
+            FileInfo[] fget = directory.GetFiles();
+            FileSystemInfo[] fss = directory.GetFileSystemInfos();
+            foreach (FileSystemInfo f in fss)
+            {
+                if (f.GetType() == typeof(DirectoryInfo))
+                {
+                    Console.WriteLine("Directory:" + f.Name);
+                }
+                else
+                {
+                    Console.WriteLine("File:" + f.FullName);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
