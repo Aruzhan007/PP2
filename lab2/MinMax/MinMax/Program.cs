d6d22fb3ebbf6c79d255de41f8d130095a368d9a
aruzhantlek <tlekovaaru@gmail.com>
2018-02-05 22:51:43 +0600

    MinMaxNum

---
+++ lab2/MinMax/MinMax/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinMax
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"/Users/aruzan/Desktop/Calculus/a.txt");
+            args = text.Split();
+            int[] n = new int[args.Count()];
+            for (int i = 0; i < args.Length;i++)
+            {
+                n[i] = int.Parse(args[i]); 
+            }
+            Console.WriteLine(n.Max());
+
+            Console.WriteLine(n.Min());
+
+            Console.ReadKey();
+        }
+    }
+}
