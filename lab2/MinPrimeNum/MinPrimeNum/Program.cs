d8f75ff02a06d95e6d4c60a3272b999cab78d25f
aruzhantlek <tlekovaaru@gmail.com>
2018-02-06 03:29:06 +0600

    Primemin

---
+++ lab2/MinPrimeNum/MinPrimeNum/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace MinPrimeNum
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> prime = new List<int>();
+            string s = File.ReadAllText(@"/Users/aruzan/Desktop/Calculus/a.txt");
+            string[] arr = s.Split(' ');
+            int[] n = new int[arr.Count()];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                n[i] = int.Parse(args[i]);
+                bool ok = true;
+
+                for (int j = 2; j*j <= n[i]; j++);
+                {
+                    if (n[i] % i == 0)
+                    {
+                        ok = false;
+                        break;
+                    }
+
+                }
+                if (ok & n[i] != 1)
+                {
+                    prime.Add(n[i]);
+                }
+
+            }
+            prime.Sort();
+            if (prime.Count == 0)
+            {
+                File.WriteAllText(@"/Users/aruzan/Desktop/Calculus/b.txt", "There aren't primes");
+            }
+            else
+            {
+                File.WriteAllText(@"/Users/aruzan/Desktop/Calculus/b.txt", Convert.ToString(prime.Min()));
+
+            }
+
+            for (int i = 0; i < prime.Count; i++)
+            {
+                if (prime.Count > 0)
+                {
+                    File.WriteAllText(@"/Users/aruzan/Desktop/Calculus/b.txt", Convert.ToString(prime[i]));
+                }
+                break;
+            }
+            Console.ReadKey();
+        }
+
+    }
+
+}
