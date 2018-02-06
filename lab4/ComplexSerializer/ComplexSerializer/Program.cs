31004f8b6454c5787f94a040714d608dfdcb4f35
aruzhantlek <tlekovaaru@gmail.com>
2018-02-07 01:39:54 +0600

    serializer

---
+++ lab4/ComplexSerializer/ComplexSerializer/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+namespace ComplexSerializer
+{
+    class Complex
+    {
+        public int a;
+        public int b;
+        public int GCD(int x, int y)
+        {
+            while (x != y)
+            {
+                if (x > y)
+                {
+                    x = x - y;
+                }
+                else
+                {
+                    y = y - x;
+                }
+            }
+            return x;
+        }
+        public Complex(int _a, int _b)
+        {
+            a = _a;
+            b = _b;
+        }
+        public Complex()
+        {
+        }
+        public void simplify()
+        {
+            int s = GCD(this.a, this.b);
+            this.a = this.a / s;
+            this.b = this.b / s;
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex n = new Complex();
+            n.a = c1.a * c2.b + c2.a * c2.b;
+            n.b = c1.b * c2.b;
+            n.simplify();
+            return n;
+        }
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            Complex n = new Complex();
+            n.a = c1.a * c2.b - c2.a * c2.b;
+            n.b = c1.b * c2.b;
+            n.simplify();
+            return n;
+        }
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("a.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Complex));
+
+            Complex c1 = new Complex(2, 4);
+            Complex c2 = new Complex(5, 2);
+            Complex add = c1 + c2;
+            Complex sub = c1 - c2;
+
+            Console.WriteLine(add);
+            Console.WriteLine(sub);
+            try
+            {
+                xs.Serialize(fs, c1);
+                xs.Serialize(fs, c2);
+
+            }
+            catch (Exception j)
+            {
+                Console.WriteLine(j.ToString());
+
+            }
+            finally
+            {
+                fs.Close();
+            }
+            Console.ReadKey();
+        }
+    }
+}
