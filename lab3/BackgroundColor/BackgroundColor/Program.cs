39024d3e45aa658a42a5f9f1d9574de03b5bf7e5
aruzhantlek <tlekovaaru@gmail.com>
2018-02-06 09:20:12 +0600

    background

---
+++ lab3/BackgroundColor/BackgroundColor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+
+namespace BackgroundColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"/Users/aruzan/Documents/programming1");
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Clear();
+
+
+            FileSystemInfo[] fget = d.GetFileSystemInfos();
+            foreach(FileSystemInfo f in fget)
+            {
+                if(f.GetType()==typeof(DirectoryInfo))
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+                }
+                if(f.GetType()==typeof(FileInfo))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.BackgroundColor = ConsoleColor.Red;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
