bd6830c5f3732ac6c4c0500d1a5419b137b4dbac
aruzhantlek <tlekovaaru@gmail.com>
2018-02-06 08:50:25 +0600

    lab3

---
+++ lab3/FarManager/FarManager/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarManager
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ConsoleKeyInfo keyInf = Console.ReadKey();
+                if (keyInf.Key == ConsoleKey.UpArrow)
+                {
+                    Console.WriteLine("Up");
+                }
+                if (keyInf.Key == ConsoleKey.DownArrow)
+                {
+                    Console.WriteLine("Down");
+                }
+                if (keyInf.Key == ConsoleKey.RightArrow)
+                {
+                    Console.WriteLine("Right");
+                }
+                if (keyInf.Key == ConsoleKey.LeftArrow)
+                {
+                    Console.WriteLine("Left");
+                }
+                if(keyInf.Key==ConsoleKey.Escape)
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
